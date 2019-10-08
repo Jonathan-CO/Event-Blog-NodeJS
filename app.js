2aa179978cca8e0b31a69b49cf9712803efcbc43
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-08 03:33:44 -0300

    separa db do app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //carregando módulos
     const express = require ('express')
     const handlebars = require ('express-handlebars')
-    const mongoose = require('mongoose')
+    
     const app = express()
     const admin = require('./routes/admin')
     const path = require('path')
 
+    const mongoose = require('./models/db')
 
  //configurações
     // express json
@@ -15,15 +16,7 @@
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
-    // mongoose
-    mongoose.connect('mongodb+srv://blog:blog@blog-pjdr7.mongodb.net/blognodejs?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then(()=>{
-        console.log("Conectado")
-    }).catch((erro)=>{
-        console.log("Erro ao conectar: "+erro)
-    })
+    
 
     //Public
     app.use(express.static(path.join(__dirname, 'public')))
