32f5740595e26daf9e163c8ddd260df2c8e6aa11
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-08 12:28:59 -0300

    cria model Categoria, rotas e variaveis globais para sessions

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 //carregando módulos
     const express = require ('express')
     const handlebars = require ('express-handlebars')
-    
     const app = express()
     const admin = require('./routes/admin')
     const path = require('path')
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
-    const mongoose = require('./models/db')
+    require('./models/db')
 
  //configurações
+    //session
+    app.use(session({
+        secret: 'cursodenode',
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(flash())
+
+    //Middleware
+    app.use((req, res, next)=>{ // res.locals cria variáveis globais
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
+ 
     // express json
     app.use(express.json())
 
@@ -16,15 +32,16 @@
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
 
-    
-
     //Public
     app.use(express.static(path.join(__dirname, 'public')))
+
 
 //rotas
 app.use('/admin', admin)
 
 //outros
+
+
 
 const port = 8081
 app.listen(port, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -10,7 +15,23 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.send("Página de categorias")
+    res.render('admin/categorias')
+})
+
+router.get('/categorias/add', (req, res) => {
+    res.render('admin/addcategorias')
+})
+
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome : req.body.nome,
+        slug: req.body.slug
+    }
+    new Categoria(novaCategoria).save().then(()=>{
+        console.log("Categoria salva")
+    }).catch((erro) => {
+        console.log("Erro ao salvar categoria: "+erro)
+    })
 })
 
 module.exports = router
