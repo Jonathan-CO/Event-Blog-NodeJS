700bacefaa19cf6b53879bcbd994ee6b71cbb837
Jonathan-CO <j_cunha_oliveira@hotmail.com>
2019-10-09 01:59:35 -0300

    delete postagens - FIM do CRUD Postagens

--- routes/admin.js
+++ routes/admin.js
@@ -209,4 +209,14 @@ router.post('/postagens/edit', (req, res)=>{
     })
 })
 
+router.get('/postagens/deletar/:id', (req, res)=>{
+    Postagem.deleteOne({_id: req.params.id}).then(()=>{
+        req.flash('success_msg', 'Postagem deletada com sucesso')
+        res.redirect('/admin/postagens')
+    }).catch((erro)=>{
+        req.flash('error_msg', 'Houve um erro ao delear a postagem')
+        res.redirect('/admin/postagens')
+    })
+})
+
 module.exports = router
